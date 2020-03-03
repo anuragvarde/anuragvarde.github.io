b859fd633b415bf722884b9424d2cdc791dd1c0e
anuragvarde <vardes99@yahoo.com>
2020-03-03 15:31:35 -0500

    Removed frame where possible
    Replaced iframe with window.open.
    Replaced frame with null.

--- markets.js
+++ markets.js
@@ -6,26 +6,26 @@
         if (localticker != "")
             switch (source) {
             case "RT":                
-                return "https://www.cnbc.com/quotes/?symbol=" + localticker;
+                return "http://www.cnbc.com/quotes/?symbol=" + localticker;
             case "ES":
-                //return "https://www.etfscreen.com/risk-return-chart.php?s=" + localticker;
+                //return "http://www.etfscreen.com/risk-return-chart.php?s=" + localticker;
                 return "https://www.etfscreen.com/risk-return-chart.php?cSyms=AOR+AOA+AOK+AOM+BND+BNDX+BSV+DBC+DIA+EMLC+IAU+IWC+IWM+MDY+MUB+PGX+RWX+SDOG+SHM+SLV+SPY+USO+VEA+VEU+VIG+VNQ+VT+VTI+VWO+VYM+XLB+XLE+XLF+XLI+XLK+XLP+XLU+XLV+XLY+&s=" + localticker;
             case "DT":
-                return "https://stockcharts.com/h-sc/ui?s=" + localticker + "&p=D&yr=0&mn=6&dy=0&id=p06749700604";
+                return "http://stockcharts.com/h-sc/ui?s=" + localticker + "&p=D&yr=0&mn=6&dy=0&id=p06749700604";
             case "OP":
-                return "https://stockcharts.com/h-sc/ui?s=" + localticker + "&p=D&yr=0&mn=6&dy=0&id=p39782870368";
+                return "http://stockcharts.com/h-sc/ui?s=" + localticker + "&p=D&yr=0&mn=6&dy=0&id=p43267027058";
             case "EL":
-                return "https://stockcharts.com/h-sc/ui?s=" + localticker + "&p=D&yr=0&mn=6&dy=0&id=p97376413214";
+                return "http://stockcharts.com/h-sc/ui?s=" + localticker + "&p=D&yr=0&mn=6&dy=0&id=p97376413214";
             case "ST":
-                return "https://stockcharts.com/h-sc/ui?s=" + localticker + "&p=D&yr=0&mn=6&dy=0&id=p71327581855";
+                return "http://stockcharts.com/h-sc/ui?s=" + localticker + "&p=D&yr=0&mn=6&dy=0&id=p71327581855";
             case "PT":
-                return "https://stockcharts.com/h-sc/ui?s=" + localticker + "&p=D&yr=0&mn=6&dy=0&id=p22761169359";
+                return "http://stockcharts.com/h-sc/ui?s=" + localticker + "&p=D&yr=0&mn=6&dy=0&id=p22761169359";
             case "SC":
-                return "https://www.stockconsultant.com/consultnow/basicplus.cgi?symbol=" + localticker;
+                return "http://www.stockconsultant.com/consultnow/basicplus.cgi?symbol=" + localticker;
             case "FV":
-                return "https://finviz.com/quote.ashx?t=" + localticker;
+                return "http://finviz.com/quote.ashx?t=" + localticker;
             default:
-                return "https://stockcharts.com/h-sc/ui?s=" + localticker + "&p=D&yr=0&mn=6&dy=0&id=p71017663931"
+                return "http://stockcharts.com/h-sc/ui?s=" + localticker + "&p=D&yr=0&mn=6&dy=0&id=p71017663931"
         }
         else {
             //alert("No Ticker")
@@ -38,7 +38,8 @@
         var URL = getSQURL(enteredticker, tickersrc);
         //alert(URL)
         if (URL != "") { 
-            $('#frame').attr('src',URL);          
+           //$('#frame').attr('src',URL);          
+           window.open(URL, '_blank');
         }
 
     };
